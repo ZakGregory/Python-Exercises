79873159510ae85744d4b36e79d9fb0b0e3c1b08
ZakGregory <zgregoryk@gmail.com>
2020-10-23 11:59:33 +0100

    Fixed tt.py

--- Easy/Times Tables.py
+++ Easy/Times Tables.py
@@ -1,9 +1,9 @@
 prt=""
 
-for i in range(0,10):
+for i in range(1,11):
     prt=""
     
     for j in range(1,11):
-        num=i*10+j
+        num=i*j
         prt=prt+" "+str(num)
     print(prt)
